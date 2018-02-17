c048265b2d4202c9dbdcd6e55df97c6117d77fd5
Jonathan Pater <pater@cowboyneal.org>
2018-02-17 04:02:07 -0500

    Add RSS button

--- beatbot.py
+++ beatbot.py
@@ -2,10 +2,15 @@ import io
 import imghdr
 
 from flask import Flask
-from flask import render_template, jsonify, send_file
+from flask import render_template, jsonify, send_file, make_response
 from musicpd import MPDClient
 from mutagen import File
+from functools import wraps, update_wrapper
+from datetime import datetime
 app = Flask(__name__)
+
+SONG_FILE_DIRECTORY = '/Users/pater/Music/Mixxx/'
+PLACEHOLDER_IMAGE = 'notfound.jpg'
 
 def get_client():
     client = MPDClient()
@@ -41,7 +46,7 @@ def mpd():
 
     list_start = str(int(current_song['pos']) + 1)
     list_end = str(min(int(current_song['pos']) + 11,
-            int(status['playlistlength'])))
+            int(status['playlistlength']) - 1))
 
     data = {
         'currentsong'  : current_song,
@@ -54,8 +59,20 @@ def mpd():
 
     return jsonify(data)
 
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+        
+    return update_wrapper(no_cache, view)
+
 def get_placeholder_image():
-    image_file = open('notfound.jpg', 'rb')
+    image_file = open(PLACEHOLDER_IMAGE, 'rb')
     image = image_file.read()
     image_file.close()
 
@@ -65,12 +82,13 @@ def get_image_type(image):
     return imghdr.what('', image)
 
 @app.route('/album_art/<int:song_id>')
+@nocache
 def album_art(song_id):
     client = get_client()
     file_name = client.playlistid(song_id)[0]['file']
     close_client(client)
 
-    song_file = File('/Users/pater/Music/Mixxx/' + file_name)
+    song_file = File(SONG_FILE_DIRECTORY + file_name)
 
     try:
         image = song_file.tags['APIC:'].data
@@ -86,3 +104,12 @@ def album_art(song_id):
     return send_file(io.BytesIO(image),
         attachment_filename=str(song_id) + '.' + image_type,
         mimetype='image/' + image_type)
+
+@app.route('/search/<string:match>')
+def search(match):
+    results = {}
+
+    client = get_client()
+    close_client(client)
+
+    return jsonify(results)
