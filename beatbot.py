4bb0433702d17421f2f4ec77bf2732189391e761
Jonathan Pater <pater@cowboyneal.org>
2018-04-13 21:43:02 +0000

    Remove sse from beatbot.py

--- beatbot.py
+++ beatbot.py
@@ -7,7 +7,6 @@ from flask import Flask, render_template, jsonify, send_file, make_response, \
         send_from_directory, request
 from flask_mobility import Mobility
 from flask_mobility.decorators import mobile_template
-from flask_sse import sse
 from musicpd import MPDClient
 from mutagen import File
 from PIL import Image
@@ -16,7 +15,6 @@ from datetime import datetime
 app = Flask(__name__)
 Mobility(app)
 app.config.from_object('config')
-app.register_blueprint(sse, url_prefix='/status')
 
 def nocache(view):
     @wraps(view)
